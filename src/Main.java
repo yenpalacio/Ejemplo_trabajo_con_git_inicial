a5ca189215e3ed1e154fbb1901dd0f1eec225af6
*yenpalacio@cesde.net* <*yenpalacio@cesde.net*>
2026-07-28 20:32:42 -0500

    Actualizacion de main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("MENU");
+        System.out.println("1. opcion");
+        System.out.println("2.opcion");
+        System.out.println("3.opcion");
     }
 }
